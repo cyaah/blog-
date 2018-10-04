af39fc7c12a172098266be524f177b5d4a57b2d1
Calvin Yah <calvin.yah@gmail.com>
2018-10-04 10:44:58 -0400

    Created post index component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { react } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 
-export default class PostsIndex extends Component {
+
+
+class PostsIndex extends Component {
+  componenetDidMount() {
+  	this.props.fetchPosts();
+  }
+
   render() {
+  	console.log(this.props.posts);
     return (
       <div>Posts Index</div>
     );
   }
 }
+
+function mapStatetoProps(state) {
+	return { posts: state.posts };	
+}
+
+export default  connect(null, { fetchPosts })(PostsIndex);
