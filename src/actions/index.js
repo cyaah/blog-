4c71bab194b8d7fca7c4552589690a7ac5b6ff56
cyaah <calvin.yah@gmail.com>
2018-10-04 20:48:47 -0400

    second commit

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+
+export const FETCH_POSTS ='fetch_posts';
+
+const ROOT_URL = "http://reduxblog.herokuapp.com/api";
+const API_KEY = "?key=cyah13";
+
+
+
+export function fetchPosts (id) {
+	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+    
+	return {
+      type: FETCH_POSTS,
+      payload: request
+	};
+}`   `
---
+++ src/components/posts_index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+import _ from 'lodash';
+
+
+class PostsIndex extends Component {
+   componentDidMount () {
+     this.props.fetchPosts();
+   }
+
+
+	render () {
+		console.log(this.props.posts);
+		return (
+          <div>
+            Posts Index 
+          </div>
+	  );
+	}
+}
+
+
+function mapStateToProps(state) {
+	return { posts: state.posts };
+
+}
+
+
+export default connect(null, { fetchPosts: fetchPosts }) (PostsIndex) ;
