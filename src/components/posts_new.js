347668a756af48dbc2c5243ddd325c7bf2d268b6
cyaah <calvin.yah@gmail.com>
2018-10-11 18:11:33 -0400

    Created redux form with 3 inputs

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
+import {Field, reduxForm } from 'redux-form';
+
+
 
 class PostsNew extends Component {
+  renderField(field){
+  	//field is essentially each Field component below
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type = "test"
+          //Below basically contains on onCHhange, onHover event handlers etc 
+          {...field.input}
+        />
+       </div>
+    );
+
+  }
+
+
+
+
   render() {
   	return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          //label can be any name eg. labelToShow etc etc
+          label="Title"
+          name="title"
+          component={this.renderField} 
+        />
+        <Field 
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+          />
+        <Field 
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+          />
+      </form>  
   	);
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+	form: 'PostsNewForm'
+}) (PostsNew);
