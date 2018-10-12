7e8697af26491750a54b6f15ade64c1811fa0ffd
cyaah <calvin.yah@gmail.com>
2018-10-11 23:18:33 -0400

    Finished creating form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -5,9 +5,15 @@ import {Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field){
-  	//field is essentially each Field component below
+    //Basically replaces all field.meta and meta.touched, meta.error so we just need to use meta. We are destructuring
+  	const { meta: { touched, error } } = field; 
+  	const className = `form-group ${touched && error ? 'has-danger': ''}`
+
+
+
+  	//field is essentially a single input or a single piece of state. i.e Field commponent on the bottom
     return (
-      <div className="form-group">
+      <div className = {className} >
         <label>{field.label}</label>
         <input
           className="form-control"
@@ -15,17 +21,34 @@ class PostsNew extends Component {
           //Below basically contains on onCHhange, onHover event handlers etc 
           {...field.input}
         />
+
+          <div className = "text-help">
+          {touched ? error: ''}
+          </div>
        </div>
     );
-
+//the question mark above (ternary operator) checks if the field has been touched if truthy returns everything between the question mark and colon and if not then returns error as ''
   }
 
+onSubmit (values) {
+	// this == component
+	console.log(values);
+}
 
 
 
   render() {
+
+  	//we got this from the bottom of the code in reduxForm 
+  	//Just es6 for const handleSubmt = this.props.handleSubmit
+    const{ handleSubmit } = this.props;
+
   	return (
-      <form>
+
+  		//handleSubmit is the redux-form side of things where we do the validation
+  		//Once redux form is ready to be submitted then it calls the function we definded and passes us values out fo the form tow ork with
+  		//We use .bind(this) cause we are passing this.onSubmit as a callback function in some different context outside our component
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           //label can be any name eg. labelToShow etc etc
           label="Title"
@@ -42,11 +65,42 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
           />
+          <button type="submit" className="btn btn-primary"> Submit </button>
       </form>  
   	);
   }
 }
 
+
+function validate(values){
+ //values is all the different values a user enters into the form
+ //console.log(values) => { title:'asd', categoies:'asdk', content:'asf'}
+ const errors = {};
+
+ //validate the inputs from the value object
+if(!values.title || values.title.length < 3) {
+	errors.title = "Enter a title longer than 3 characters";
+}
+
+// if(values.title.length < 3) {
+// 	errors.title = "Title must be atleast 3 characters long";
+// }
+
+if(!values.categories){
+	errors.categories = "Enter a category"
+}
+
+if(!values.content) {
+	errors.content = "Enter some content"
+}
+//if errors is empty, the form is fine to submit
+//if errors has any properties, redux form assumes form is invalid
+ return errors;
+}
+
+
+
 export default reduxForm({
+	validate: validate,
 	form: 'PostsNewForm'
 }) (PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,7 @@
 import { combineReducers } from "redux";
 import  postReducer  from './reducer_posts';
 import { reducer as formReducer } from 'redux-form';
-<<<<<<< HEAD
-=======
 
->>>>>>> e51dea6f0ba7b6f99ca1b4ae0e3ad06f571cf1a4
 
 const rootReducer = combineReducers({
   posts: postReducer,
